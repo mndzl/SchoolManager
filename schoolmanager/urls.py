357d2a739bf5f296d8d84fab45d7d5d3192bfc94
Luis Mendez <mendezgla.56@gmail.com>
2020-03-29 18:50:57 -0300

    tasks, css and responsive

--- schoolmanager/urls.py
+++ schoolmanager/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from task.views import reminders
+from task import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import path
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', reminders)
+    path('admin/', admin.site.urls),
+    path('', views.reminders),
+    path('tasks/', views.tasks)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- task/views.py
+++ task/views.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def reminders(request):
     return render(request, 'task/reminders.html')
+
+def tasks(request):
+    return render(request, 'task/tasks.html')
