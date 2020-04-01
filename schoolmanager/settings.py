3c65c079956e63c672971cb062e8a0d21fe626d2
Luis Mendez <mendezgla.56@gmail.com>
2020-04-01 14:26:55 -0300

    database configuration

--- schoolmanager/settings.py
+++ schoolmanager/settings.py
@@ -84,18 +84,11 @@ WSGI_APPLICATION = 'schoolmanager.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {    
-    'default': {        
-        'ENGINE': 'django.db.backends.sqlite3',        
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),    
-    }
-}
-"""
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
-}"""
+}
 
 
 
