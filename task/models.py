b0071e001b0f597c3a48620007f662982a1f3c87
Luis Mendez <mendezgla.56@gmail.com>
2020-04-08 18:05:39 -0300

    help me please again

--- task/models.py
+++ task/models.py
@@ -10,7 +10,7 @@ class Subject(models.Model):
     teacher = models.CharField(max_length=50)
     schedule = models.FileField(null=True, blank=True, upload_to='media/')
     days = models.CharField(max_length=100, default='none', blank=True, null=True)
-    #grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class Task(models.Model):
     description = models.TextField(blank=True, null=True, max_length=1000)
     type_task = models.CharField(max_length=20)
     subject = models.ForeignKey(Subject, blank=True, null=True, on_delete=models.CASCADE)
-    #grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=0)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=0)
 
     def __str__(self):
         return self.title + " ({})".format(self.type_task)
@@ -29,7 +29,7 @@ class Task(models.Model):
 class File(models.Model):
     file_path = models.FileField(upload_to='media/')
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    #grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.file_path.name + " ({})".format(self.task.title) 
@@ -38,7 +38,7 @@ class Done(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     time = models.DateTimeField(default=timezone.now)
-    #grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.task.title + " ({})".format(self.user.username)
